257d75fbe76367fd4593263446855dd310272967
GITA <jgitanjali592@gmail.com>
2024-06-05 18:00:01 +0530

    this code for module concept

---
+++ Module/result.js
@@ -0,0 +1,12 @@
+// call arithmatic functions get result and print on terminal .
+
+
+// how to import a module
+
+const arithmaticModule = require('./arithmatic');
+
+// call sum function
+
+const result = arithmaticModule.sum(10, 20);
+
+console.log(result);
