9f113d78e7349b4a3ceb88dbe6c2aa78cb4c77df
GITA <jgitanjali592@gmail.com>
2024-06-06 11:08:09 +0530

    this code for ES6

---
+++ ES6 Module/arithmatic.mjs
@@ -0,0 +1,7 @@
+// using ES6 syntax , export module
+
+export function sum(a,b){
+  return a + b;
+}
+
+export const num =100;
---
+++ ES6 Module/result.mjs
@@ -0,0 +1,8 @@
+import * as arithmaticModule from './arithmatic.mjs';
+import  {sum} from './arithmatic.mjs';
+
+// console.log(arithmaticModule.sum(10,20));
+// console.log(arithmaticModule.num);
+
+console.log(sum(10,20));
+console.log(arithmaticModule.num);
