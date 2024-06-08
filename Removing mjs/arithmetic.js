e063b54c7ccc80dfdf13345551f98e0fbdb0514f
GITA <jgitanjali592@gmail.com>
2024-06-08 09:09:12 +0530

    this code for removing mjs create module type

---
+++ Removing mjs/arithmetic.js
@@ -0,0 +1,10 @@
+export function sum(a,b){
+  return a+ b;
+
+
+}
+
+export const num =100;
+function div(a,b){
+  return a/b;
+}
---
+++ Removing mjs/result.js
@@ -0,0 +1,3 @@
+import * as arithmetic from  './arithmetic.js'
+
+console.log(arithmetic.sum(10,20));
