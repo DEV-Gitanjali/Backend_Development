8a62ef44c75f1dbd07bc84a57b0c63ee5a3f2309
GITA <jgitanjali592@gmail.com>
2024-06-10 12:42:53 +0530

    this code for path module

--- Reading Command line/FileModule/fileOperationsAsync.js
+++ Reading Command line/FileModule/fileOperationsAsync.js
@@ -1,54 +1,79 @@
-const fs= require('fs')
+// const fs= require('fs')
 
-//reading data
+// //reading data
 
-// fs.readFile('data.txt' , (err , data)=>{
+// // fs.readFile('data.txt' , (err , data)=>{
+// //   if(err){
+// //     console.log(err);
+// //   }
+
+// //   else {
+// //     console.log(data.toString());
+// //   }
+// // })
+
+
+
+// //writing data
+
+// fs.writeFile('employee2.txt' , 'new Employee' , (err )=>{
 //   if(err){
 //     console.log(err);
 //   }
 
-//   else {
-//     console.log(data.toString());
+//   else{
+//     console.log('data written');
+//   }
+// })
+
+// // append data
+
+// fs.appendFile('employee2.txt' , '\n anther operation' , (err)=>{
+//   if(err){
+//     console.log(err);
+//   }
+
+//   else{
+//     console.log('updated file');
+//   }
+// })  
+
+
+// // delete a file 
+// fs.unlink("employee.text" , (err)=>{
+//   if(err){
+//     console.log(err);
+//   }
+
+//   else{
+//     console.log('file deleted');
 //   }
 // })
 
 
+// console.log('this is another operation')
 
-//writing data
+// path module
 
-fs.writeFile('employee2.txt' , 'new Employee' , (err )=>{
+const fs = require('fs');
+const path = require('path');
+
+//radding data
+
+const filePath= path.join('src' , 'home' , 'data.txt');
+const filePathResolved= path.resolve('src' , 'home' , 'data.txt');
+
+console.log(filePath);
+console.log(filePathResolved);
+console.log(path.extname(filePathResolved))
+
+fs.readFile(filePath, (err, data)=>{
   if(err){
-    console.log(err);
+    console.log(err)
   }
-
   else{
-    console.log('data written');
+    console.log(data.toString());
   }
-})
+}
+)
 
-// append data
-
-fs.appendFile('employee2.txt' , '\n anther operation' , (err)=>{
-  if(err){
-    console.log(err);
-  }
-
-  else{
-    console.log('updated file');
-  }
-})  
-
-
-// delete a file 
-fs.unlink("employee.text" , (err)=>{
-  if(err){
-    console.log(err);
-  }
-
-  else{
-    console.log('file deleted');
-  }
-})
-
-
-console.log('this is another operation')
