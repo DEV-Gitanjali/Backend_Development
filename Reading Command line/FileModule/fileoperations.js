8b07ceca8171e5b9f1fa4ca02a2a315c6ca8a28b
GITA <jgitanjali592@gmail.com>
2024-06-10 10:04:31 +0530

    this code for create a new file & write something

--- Reading Command line/FileModule/fileoperations.js
+++ Reading Command line/FileModule/fileoperations.js
@@ -2,10 +2,20 @@ const fs = require('fs');
 
 // TO read file content using nlocking code
 
-console.log('starting to read ');
+// console.log('starting to read ');
 
-const buffer = fs.readFileSync('data.txt' ,{encoding:'utf-8'})
+// const buffer = fs.readFileSync('data.txt' ,{encoding:'utf-8'})
 
-console.log(buffer);
+// console.log(buffer);
+
+//  creating and writting a file
+try{
+
+  fs.writeFileSync('employee.text' , 'Name:subham pradhan , Age:22 , posstion:Software engineer');
+
+  } catch(err){
+    console.log(err);
+  }
+ 
 
 console.log('This is another operation being performed');
