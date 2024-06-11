a11ed72ba65ddd67700c2fe67a96f6b3764e8b54
GITA <jgitanjali592@gmail.com>
2024-06-11 10:39:33 +0530

    this code for index get server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express= require('express');
+
+const server = express();
+server.get('/' , (req, res)=>{
+  res.send('Be a coding ninja');
+})
+
+
+server.listen(3000 , ()=>{
+  console.log('listening to port 3000')
+})
