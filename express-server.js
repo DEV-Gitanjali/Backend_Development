fc2228a1aad3f66ce19db3e89a98d5c7a3204d25
GITA <jgitanjali592@gmail.com>
2024-06-15 23:27:32 +0530

    this code for multiple middleware

--- express-server.js
+++ express-server.js
@@ -3,15 +3,28 @@ const express = require('express');
 const server =  express();
 
 // handle default request
-server.get('/' , 
-  // 1st middleware
-  (req , res , next)=>{
-  // res.send(' this is first middleware');
+// server.get('/' , 
+//   // 1st middleware
+//   (req , res , next)=>{
+//   // res.send(' this is first middleware');
+//   console.log('first middleware');
+//   next();
+// })
+// multiple middleware
+
+function firstMiddleware(req, res, next){
   console.log('first middleware');
   next();
-})
+}
+function secondMiddleware(req, res, next){
+  console.log('second middleware');
+  next();
+}
+
+
 // 2nd middleware
-  server.get( '/', (req , res)=>{
+  server.get( '/',[firstMiddleware , secondMiddleware] ,(req , res)=>{
+
   res.send('welcome to express server');
 });
 
