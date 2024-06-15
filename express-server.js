9c8a0db17d25def7939b0d0780af1655f2525c92
GITA <jgitanjali592@gmail.com>
2024-06-15 22:03:27 +0530

    this code for middleware

--- express-server.js
+++ express-server.js
@@ -3,11 +3,19 @@ const express = require('express');
 const server =  express();
 
 // handle default request
-server.get('/' , (req , res)=>{
+server.get('/' , 
+  // 1st middleware
+  (req , res , next)=>{
+  // res.send(' this is first middleware');
+  console.log('first middleware');
+  next();
+})
+// 2nd middleware
+  server.get( '/', (req , res)=>{
   res.send('welcome to express server');
 });
 
 // listen on specified  port 
 server.listen(3100 , ()=>{
-  console.log('listening to port 3100');
+  console.log(`server running at http://localhost:3200`);
 })
